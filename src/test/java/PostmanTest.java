2ec355e8fd254ef26cc925bda18899528710b22a
Olga Vlasova <olgav1918@gmail.com>
2023-06-23 20:49:47 +0300

    initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void shouldReturnSentBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi, Olga")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi,Olga"));
+
+    }
+ }
